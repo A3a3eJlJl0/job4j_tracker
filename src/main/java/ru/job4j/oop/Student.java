e52f6645b7b88d6965b18b1c02f7866298c9b338
A3a3eJlJlo <afonchenko.web@gmail.com>
2021-04-04 21:57:25 +0300

    1.2. Вызов метода объекта [#503458]

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -11,10 +11,12 @@ public class Student {
 
     public static void main(String[] args) {
         Student petya = new Student();
-        for (int i = 0; i < 3; i++) {
-            petya.music();
-            petya.song();
-        }
+        petya.music();
+        petya.music();
+        petya.music();
+        petya.song();
+        petya.song();
+        petya.song();
     }
 }
 
