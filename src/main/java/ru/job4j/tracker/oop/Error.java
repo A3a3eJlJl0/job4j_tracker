0e7e84876550000c2d306f654706404b31cda1f0
A3a3eJlJlo <afonchenko.web@gmail.com>
2021-04-04 18:45:46 +0300

    1.7. Конструктор [#503463]

---
+++ src/main/java/ru/job4j/tracker/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.tracker.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {}
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printError() {
+        System.out.println("Active: " + this.active);
+        System.out.println("Status: " + this.status);
+        System.out.println("Message: " + this.message);
+    }
+
+    public static void main(String[] args) {
+        Error error1 = new Error();
+        Error error2 = new Error(true, 2, "Division by zero");
+        Error error3 = new Error(false, 5, "Low memory");
+
+        error1.printError();
+        error2.printError();
+        error3.printError();
+    }
+}
