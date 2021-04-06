4d15a6aa88b3691b1dfb5e9cdfa490ed8858fac9
A3a3eJlJlo <afonchenko.web@gmail.com>
2021-04-06 20:31:51 +0300

    9. toString [#503483]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,11 +5,7 @@ import java.time.format.DateTimeFormatter;
 
 public class StartUI {
     public static void main(String[] args) {
-        Tracker tracker = new Tracker();
-        tracker.add(new Item("Test"));
-        Item item = tracker.findById(1);
-        System.out.println(item.getId());
-        System.out.println(item.getName());
-        System.out.println(item.getCreated());
+        Item item = new Item("Apple");
+        System.out.println(item);
     }
 }
