a9dd780e6d87e778faf1c256537ed2278199b11f
Евгений Афонченко <afonchenko.web@gmail.com>
2021-04-15 12:24:10 +0300

    9.1 Зависимость от System.out [#503396]

--- src/main/java/ru/job4j/tracker/GetByIdAction.java
+++ src/main/java/ru/job4j/tracker/GetByIdAction.java
@@ -1,6 +1,12 @@
 package ru.job4j.tracker;
 
 public class GetByIdAction implements UserAction{
+    private final Output out;
+
+    public GetByIdAction(Output out) {
+        this.out = out;
+    }
+
     @Override
     public String name() {
         return "Find item by ID";
