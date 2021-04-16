98ea98149be610154bf84da6e930f634e77822c4
A3a3eJlJlo <afonchenko.web@gmail.com>
2021-04-16 22:51:55 +0300

    0.5. Иерархия исключений и множественный catch. [#503428]

--- src/main/java/ru/job4j/ex/User.java
+++ src/main/java/ru/job4j/ex/User.java
@@ -1,4 +1,4 @@
-package ru.job4j.tracker;
+package ru.job4j.ex;
 
 public class User {
     private String username;
--- src/main/java/ru/job4j/ex/UserInvalidException.java
+++ src/main/java/ru/job4j/ex/UserInvalidException.java
@@ -1,4 +1,4 @@
-package ru.job4j.tracker;
+package ru.job4j.ex;
 
 public class UserInvalidException extends UserNotFoundException{
     public UserInvalidException(String message) {
