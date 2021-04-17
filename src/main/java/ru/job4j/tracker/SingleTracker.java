9502d07b0c824b2d031aeef24f526d7aa241c848
A3a3eJlJlo <afonchenko.web@gmail.com>
2021-04-17 17:57:15 +0300

    2. Singleton [#503441]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -1,36 +1,39 @@
 package ru.job4j.tracker;
 
 public final class SingleTracker {
-    private static Tracker tracker;
+    private Tracker tracker = new Tracker();
+    private static SingleTracker single;
 
-    public static Tracker getTracker() {
-        if (tracker == null) {
-            tracker = new Tracker();
+    private SingleTracker() {}
+
+    public static SingleTracker getTracker() {
+        if (single == null) {
+            single = new SingleTracker();
         }
-        return tracker;
+        return single;
     }
 
-    public static Item add(Item item) {
-        return getTracker().add(item);
+    public Item add(Item item) {
+        return tracker.add(item);
     }
 
-    public static Item findById(int id) {
-        return getTracker().findById(id);
+    public Item findById(int id) {
+        return tracker.findById(id);
     }
 
-    public static Item[] findByName(String key) {
-        return getTracker().findByName(key);
+    public Item[] findByName(String key) {
+        return tracker.findByName(key);
     }
 
-    public static Item[] findAll() {
-        return getTracker().findAll();
+    public Item[] findAll() {
+        return tracker.findAll();
     }
 
-    public static boolean replace(int id, Item item) {
-       return getTracker().replace(id, item);
+    public boolean replace(int id, Item item) {
+       return tracker.replace(id, item);
     }
 
-    public static boolean delete(int id) {
-        return getTracker().delete(id);
+    public boolean delete(int id) {
+        return tracker.delete(id);
     }
 }
